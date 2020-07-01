803e1c351f83f8caf684dc3ebba68224e7c120c7
clyki <67692917+clyki@users.noreply.github.com>
2020-07-01 10:50:13 -0300

    Update login.php

--- login.php
+++ login.php
@@ -1,28 +1,122 @@
-<?php
-$error = false;
-if(isset($_POST['login'])){
-	$username = preg_replace('/[^A-Za-z]/', '', $_POST['username']);
-	$password = md5($_POST['password']);
-	if(file_exists('users/' . $username . '.xml')){
-		$xml = new SimpleXMLElement('users/' . $username . '.xml', 0, true);
-		if($password == $xml->password){
-			session_start();
-			$_SESSION['username'] = $username;
-			header('Location: index.php');
-			die;
-		}
-	}
-	$error = true;
-}
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+
+<!DOCTYPE html>
+<html>
 <head>
-	<title>Login</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<style>
+* {
+  box-sizing: border-box;
+}
+
+.row::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+
+[class*="col-"] {
+  float: left;
+  padding: 15px;
+}
+
+html {
+  font-family: "Lucida Sans", sans-serif;
+}
+
+.header {
+  background-color: #003399;
+  color: #ffffff;
+  padding: 15px;
+}
+
+.menu ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+
+.menu li {
+  padding: 8px;
+  margin-bottom: 7px;
+  background-color: #33b5e5;
+  color: #ffffff;
+  box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+}
+
+.menu li:hover {
+  background-color: #0099cc;
+}
+
+.aside {
+  background-color: #33b5e5;
+  padding: 15px;
+  color: #ffffff;
+  text-align: center;
+  font-size: 14px;
+  box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+}
+
+.footer {
+  background-color: #0099cc;
+  color: #ffffff;
+  text-align: center;
+  font-size: 12px;
+  padding: 15px;
+}
+
+/* For mobile phones: */
+[class*="col-"] {
+  width: 100%;
+}
+
+@media only screen and (min-width: 600px) {
+  /* For tablets: */
+  .col-s-1 {width: 8.33%;}
+  .col-s-2 {width: 16.66%;}
+  .col-s-3 {width: 25%;}
+  .col-s-4 {width: 33.33%;}
+  .col-s-5 {width: 41.66%;}
+  .col-s-6 {width: 50%;}
+  .col-s-7 {width: 58.33%;}
+  .col-s-8 {width: 66.66%;}
+  .col-s-9 {width: 75%;}
+  .col-s-10 {width: 83.33%;}
+  .col-s-11 {width: 91.66%;}
+  .col-s-12 {width: 100%;}
+}
+@media only screen and (min-width: 768px) {
+  /* For desktop: */
+  .col-1 {width: 8.33%;}
+  .col-2 {width: 16.66%;}
+  .col-3 {width: 25%;}
+  .col-4 {width: 33.33%;}
+  .col-5 {width: 41.66%;}
+  .col-6 {width: 50%;}
+  .col-7 {width: 58.33%;}
+  .col-8 {width: 66.66%;}
+  .col-9 {width: 75%;}
+  .col-10 {width: 83.33%;}
+  .col-11 {width: 91.66%;}
+  .col-12 {width: 100%;}
+}
+.Estilo1 {color: #000000}
+</style>
 </head>
 <body>
-	<h1>Login</h1>
+
+<div class="header">
+  <h1>Clyki Firewall </h1>
+</div>
+
+<div class="row">
+  <div class="col-3 col-s-3 menu">
+    <ul>
+      <li><a href="register.php" class="Estilo1">Criar Conta </a></li>
+    </ul>
+  </div>
+
+  <div class="col-6 col-s-9">
+	<h1>Entrar</h1>
 	<form method="post" action="">
 		<p>Username <input type="text" name="username" size="20" /></p>
 		<p>Password <input type="password" name="password" size="20" /></p>
@@ -33,6 +127,24 @@ if(isset($_POST['login'])){
 		?>
 		<p><input type="submit" value="Login" name="login" /></p>
 	</form>
-	<a href="register.php">Register</a>
+  <a href="register.php"></a>  </div>
+
+  <div class="col-3 col-s-12">
+    <div class="aside">
+      <h2>What?</h2>
+      <p>Chania is a city on the island of Crete.</p>
+      <h2>Where?</h2>
+      <p>Crete is a Greek island in the Mediterranean Sea.</p>
+      <h2>How?</h2>
+      <p>You can reach Chania airport from all over Europe.</p>
+    </div>
+  </div>
+</div>
+
+<div class="footer">
+  <div>&copy; Copyright Clyki firewall - 2020</div>
+  <p>&nbsp;</p>
+</div>
+
 </body>
 </html>
